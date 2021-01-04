9c183f2ea185d4fbcf5b210f3ac299bcdba60e79
lamaxiyasit <1143138206@qq.com>
2021-01-04 19:30:03 +0800

    2.2开发注册功能提交

---
+++ src/main/java/com/nowcoder/community/util/CommunityConstant.java
@@ -0,0 +1,10 @@
+package com.nowcoder.community.util;
+
+public interface CommunityConstant {
+    //激活成功
+    int ACTIVATION_SUCCESS=0;
+    //激活成功
+    int ACTIVATION_REPEAT=1;
+    //激活成功
+    int ACTIVATION_FAILURE=2;
+}
---
+++ src/main/java/com/nowcoder/community/util/CommunityUtil.java
@@ -0,0 +1,24 @@
+package com.nowcoder.community.util;
+
+import org.springframework.util.DigestUtils;
+import org.springframework.util.StringUtils;
+
+import java.util.UUID;
+
+public class CommunityUtil {
+
+    //生成随机字符串
+    public static String generateUUID(){
+        return UUID.randomUUID().toString().replaceAll("-","");
+    }
+
+    //MD5加密
+    public static String md5(String key){
+        if(StringUtils.isEmpty(key)){
+            return null;
+        }
+        else{
+            return DigestUtils.md5DigestAsHex(key.getBytes());
+        }
+    }
+}
